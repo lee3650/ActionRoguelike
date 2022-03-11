e9d48b87f561a804f6ad1c3a1b34de162ae3d6c3
Isaac Lee <lee3650@purdue.edu>
2022-03-10 23:19:19 -0500

    Modified shake screen and hitstop

--- Assets/TextFiles/Scripts/Player/ShakeScreenOnKill.cs
+++ Assets/TextFiles/Scripts/Player/ShakeScreenOnKill.cs
@@ -2,17 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShakeScreenOnKill : HitNotifier
+public class ShakeScreenOnKill : HitNotifier, LateInitializable
 {
     [SerializeField] FollowTransform Camera;
     [SerializeField] float shakeLength;
     [SerializeField] float shakeAmt;
+    [SerializeField] float timeScale;
+    [SerializeField] float stopLength;
+    [SerializeField] bool stopOnKill;
+    [SerializeField] bool shakeOnHit;
+    [SerializeField] bool stopOnDamage;
+
+    [SerializeField] HealthManager hm;
+
+    public void LateInit()
+    {
+        hm.DamageTaken += DamageTaken;
+    }
+
+    private void DamageTaken()
+    {
+        if (stopOnDamage)
+        {
+            StopAllCoroutines();
+            StartCoroutine(ScreenStop());
+        }
+    }
 
     public override void OnHit(Targetable hit)
     {
         if (hit != null && !hit.IsAlive())
         {
-            Camera.ApplyShake(shakeAmt, shakeLength);
+            if (stopOnKill)
+            {
+                StopAllCoroutines();
+                StartCoroutine(ScreenStop());
+            }
+
+            if (shakeOnHit)
+            {
+                Camera.ApplyShake(shakeAmt, shakeLength);
+            }
         }
+    }
+
+    IEnumerator ScreenStop()
+    {
+        Time.timeScale = timeScale;
+        yield return new WaitForSecondsRealtime(stopLength);
+        Time.timeScale = 1;
     }
 }
